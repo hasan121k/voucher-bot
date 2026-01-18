62109adda0ddb4e9b11d8ff7a6ddbb101d008801
hasan121k <habibhasan567890habib@gmail.com>
2026-01-18 13:12:18 +0600

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = "8347757978:AAFTmKq9GQvO-dgZlW5dx2LLkqYl1hJYbDs"
+
+PASSWORD_PREFIX = "monirul"
+TIMEZONE = "Asia/Dhaka"
+
+WEB_ADMIN_USER = "admin"
+WEB_ADMIN_PASS = "admin123"
